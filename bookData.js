4ec1500f831853ab4d21ee173c6b176a8e199766
Md. Sazzadur Rahman Zitu <37339951+SrZitu@users.noreply.github.com>
2020-08-07 23:48:17 +0600

    Create bookData.js

---
+++ bookData.js
@@ -0,0 +1,23 @@
+const books = [
+  {
+    id: 1,
+    book: "book number one",
+    author: "john milton",
+  },
+  {
+    id: 2,
+    book: "book number two",
+    author: "John lilley",
+  },
+  {
+    id: 3,
+    book: "book number three",
+    author: "Greene",
+  },
+  {
+    id: 4,
+    book: "book number four",
+    author: "Shyle",
+  },
+];
+export default books;
