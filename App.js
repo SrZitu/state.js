310505ad9e16abf8a6f3194cf59069db5f5ca6b1
Md. Sazzadur Rahman Zitu <37339951+SrZitu@users.noreply.github.com>
2020-08-08 01:11:36 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-//named and default import/exports
-//only one default export module
-//think of module as file
 import React from "react";
 import BookList from "./mycomponent/BookList";
 import "./App.css";
@@ -10,44 +7,4 @@ const App = () => (
   </div>
 );
 
-export default App;
-
-
-//import Wellcome from "./mycomponent/Wellcome";
-/*import { name, age, person } from "./mycomponent/data";
-
-
-const App = () => {
-  return (
-    <div>
-      <Banner />
-      <p>this is my content</p>
-      <p>{name}</p>
-      <p>{age}</p>
-      <p>{person.name}</p>
-      <p>{person.age}</p>
-      <hr />
-    </div>
-  );
-};*/
-//this is another category*/
-
-/***class base component*/
-
-/*class App extends Component {
-  render() {
-    return (
-      <div>
-        <Banner />
-        <p>this is my content</p>
-        <p>{name}</p>
-        <p>{age}</p>
-        <p>{person.name}</p>
-        <p>{person.age}</p>
-        <hr />
-      </div>
-    );
-  }
-}
-*/
 export default App;
