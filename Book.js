b802ffa66f2b0c24fb2b6af523e0cf2fc118f68a
Md. Sazzadur Rahman Zitu <37339951+SrZitu@users.noreply.github.com>
2020-08-08 01:13:05 +0600

    Update Book.js

--- Book.js
+++ Book.js
@@ -3,11 +3,14 @@ import React, { Component } from "react";
 export default class Book extends Component {
   render() {
     //console.log(this.props);
-    const { book, author } = this.props.info;
+    const { img, title, author } = this.props.info;
     return (
-      <div>
-        <h3>Book:{book}</h3>
-        <h3>Author:{author}</h3>
+      <div className="book">
+        <img src={img} width="150" alt="book" />
+        <div>
+          <h4>Title:{title}</h4>
+          <h6>Author:{author}</h6>
+        </div>
       </div>
     );
   }
