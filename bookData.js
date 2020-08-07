73273bfb461bc8d5d1df4c2d11edb704a54af02e
Md. Sazzadur Rahman Zitu <37339951+SrZitu@users.noreply.github.com>
2020-08-08 01:25:10 +0600

    Update bookData.js

--- bookData.js
+++ bookData.js
@@ -1,23 +1,45 @@
 const books = [
   {
     id: 1,
-    book: "book number one",
-    author: "john milton",
+    img: "https://m.media-amazon.com/images/I/91bfXqiZERL._AC_UY218_.jpg",
+    title: "Sleepers(John Milton Thrillers Book 13)",
+    author: "by john milton",
   },
   {
     id: 2,
-    book: "book number two",
-    author: "John lilley",
+    img: "https://m.media-amazon.com/images/I/A1XbpUTAKLL._AC_UY218_.jpg",
+    title: "Poems (Vintage Classics)",
+    author: "by William Blake",
   },
   {
     id: 3,
-    book: "book number three",
-    author: "Greene",
+    img: "https://m.media-amazon.com/images/I/71dUEXcxJzL._AC_UY218_.jpg",
+    title: "The Old Man and The Sea",
+    author: "by Ernest Hemingwaye",
   },
   {
     id: 4,
-    book: "book number four",
-    author: "Shyle",
+    img: "https://m.media-amazon.com/images/I/71p0EiwJ4kL._AC_UY218_.jpg",
+    title: "Selected Poems",
+    author: "by William Wordsworth ",
+  },
+  {
+    id: 5,
+    img: "https://m.media-amazon.com/images/I/81TJvpHJnpL._AC_UY218_.jpg",
+    title: "Selected Poems",
+    author: "by john milton ",
+  },
+  {
+    id: 6,
+    img: "https://m.media-amazon.com/images/I/81hdul6fy6L._AC_UY218_.jpg",
+    title: "Selected Poems",
+    author: "by john milton ",
+  },
+  {
+    id: 7,
+    img: "https://m.media-amazon.com/images/I/81rbssQxlcL._AC_UY218_.jpg",
+    title: "Selected Poems",
+    author: "by john milton ",
   },
 ];
 export default books;
