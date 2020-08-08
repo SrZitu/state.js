81e71ef1ef01836c66f7c481ecd49080ba9804bd
Md. Sazzadur Rahman Zitu <37339951+SrZitu@users.noreply.github.com>
2020-08-08 11:27:43 +0600

    Update Book.js

--- Book.js
+++ Book.js
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
 
 export default class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+      name: "john",
+    };
+    //this.handleClick = this.handleClick.bind(this);
+  }
+  /*handleClick() {
+    console.log("button is clicked");
+    console.log(this.state.count);
+  }
+  handleClick = () => {
+    console.log("button is clicked");
+    console.log(this.state.count);
+  };
+    <button type="button" onClick={this.handleClick}>
+  Add Count
+          </button>*/
+  addCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  lowerCount = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  restCount = () => {
+    this.setState({ count: 0 });
+  };
   render() {
     //console.log(this.props);
     const { img, title, author } = this.props.info;
@@ -10,6 +39,17 @@ export default class Book extends Component {
         <div>
           <h4>Title:{title}</h4>
           <h6>Author:{author}</h6>
+          <h6>Count:{this.state.count}</h6>
+          <h6>name:{this.state.name}</h6>
+          <button type="button" onClick={this.addCount}>
+            Add Count
+          </button>
+          <button type="button " onClick={this.lowerCount}>
+            Lower Count
+          </button>
+          <button type="button" onClick={this.restCount}>
+            Reset Count
+          </button>
         </div>
       </div>
     );
